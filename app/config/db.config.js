26c08eb7d2eeb2e551441bec7189853588c26b96
jayeshtharani <jayeshtharani@gmail.com>
2022-12-26 17:03:22 +0530

    changes3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,27 +1,27 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "JVKH99",
-  DB: "docmanager",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-
 //module.exports = {
-//    HOST: "3.110.168.159",
-//    USER: "harry",
-//    PASSWORD: "password",
-//    DB: "docmanager",
-//    dialect: "mysql",
-//    pool: {
-//        max: 5,
-//        min: 0,
-//        acquire: 30000,
-//        idle: 10000
-//    }
+//  HOST: "localhost",
+//  USER: "root",
+//  PASSWORD: "JVKH99",
+//  DB: "docmanager",
+//  dialect: "mysql",
+//  pool: {
+//    max: 5,
+//    min: 0,
+//    acquire: 30000,
+//    idle: 10000
+//  }
 //};
+
+module.exports = {
+    HOST: "3.110.168.159",
+    USER: "harry",
+    PASSWORD: "password",
+    DB: "docmanager",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
