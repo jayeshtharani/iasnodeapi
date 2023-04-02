d2720b676f06a15b1eb314006e993b269da53d8b
jayeshtharani <jayeshtharani@gmail.com>
2023-04-02 14:06:52 +0530

    https changes 3

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //https://codeforgeek.com/a-guide-to-securing-node-js-applications/
 const https = require("https");
+var http = require('http');
 const express = require("express");
+var privateKey = fs.readFileSync('/etc/nginx/ssl/indo-aerospace.key', 'utf8');
+var certificate = fs.readFileSync('/etc/nginx/ssl/indo-aerospace.com.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 const fs = require('fs');
 const cors = require("cors");
 const helmet = require('helmet');
@@ -62,13 +66,18 @@ require('./app/routes/customer.routes')(app);
 require('./app/routes/admin.routes')(app);
 require('./app/routes/metal.routes')(app);
 
+httpServer.listen(8080);
+httpsServer.listen(8443);
 
-https.createServer({
-    key: fs.readFileSync("/etc/nginx/ssl/indo-aerospace.key"),
-    cert: fs.readFileSync("/etc/nginx/ssl/indo-aerospace.com.crt"),
-},app).listen(443, () => {
-    console.log("server is runing at port 443");
-});
+//var httpServer = http.createServer(app);
+//var httpsServer = https.createServer(credentials, app);
+
+//https.createServer({
+//    key: fs.readFileSync("/etc/nginx/ssl/indo-aerospace.key"),
+//    cert: fs.readFileSync("/etc/nginx/ssl/indo-aerospace.com.crt"),
+//},app).listen(443, () => {
+//    console.log("server is runing at port 443");
+//});
 
 //app.listen(PORT, (err) => {
 //    if (err) console.log(err);
