ec04c2594d6553bdbe0863c2102677e348c1b1df
jayeshtharani <jayeshtharani@gmail.com>
2023-04-02 16:11:55 +0530

    https 9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const https = require("https");
 var http = require('http');
 const express = require("express");
 const fs = require('fs');
-var privateKey = fs.readFileSync('/var/www/htmlindo-aerospace.key', 'utf8');
+var privateKey = fs.readFileSync('/var/www/html/indo-aerospace.key', 'utf8');
 var certificate = fs.readFileSync('/var/www/html/indo-aerospace.com.crt', 'utf8');
 var credentials = { key: privateKey, cert: certificate };
 
